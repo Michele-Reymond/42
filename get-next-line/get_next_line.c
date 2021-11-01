7afea7d8ecbacb20fe2a23cd18e70aa114820fc6
Michele Reymond <mreymond@c1r9s4.42lausanne.ch>
2021-11-01 18:17:26 +0100

    debut de get next line

---
+++ get-next-line/get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mreymond <mreymond@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 16:04:27 by mreymond          #+#    #+#             */
+/*   Updated: 2021/11/01 18:16:23 by mreymond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	str[BUFFER_SIZE];
+
+	str = NULL;
+	read(fd, str, BUFFER_SIZE);
+}
+
+// Write a function which returns a line read from a file descriptor
+
+// PARAM
+// File descriptor to read from
+
+// RETURN
+// Read line:  correct behavior
+// NULL: nothing else to read or an error occurred
+
+// FUNCTIONS
+// read, malloc, free
